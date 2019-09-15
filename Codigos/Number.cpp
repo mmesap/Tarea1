d7443da2ea522bd8ff38d1a42f7c7fe97446e6c3
mmesap <54986519+mmesap@users.noreply.github.com>
2019-09-15 13:53:53 -0500

    Codigo 1

---
+++ Codigos/Number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+   double a;
+   std::cout << "Ingrese el numero que desea duplicar:" << "\n";
+   std::cin >> a;
+   std::cout << "El resultado es:" << "\n";
+   std::cout << 2*a << "\n";
+
+   return 0;
+}
